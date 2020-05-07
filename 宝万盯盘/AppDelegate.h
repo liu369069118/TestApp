437391295c06a90e281710b102e5b502b7a0c707
xinfeng <xf93328@163.com>
2020-05-07 14:51:54 +0800

    Initial Commit

---
+++ 宝万盯盘/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  宝万盯盘
+//
+//  Created by 辛峰 on 2020/5/7.
+//  Copyright © 2020 宝万盯盘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ 宝万盯盘/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  宝万盯盘
+//
+//  Created by 辛峰 on 2020/5/7.
+//  Copyright © 2020 宝万盯盘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
